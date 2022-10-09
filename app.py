3ae07951cba4ffdd0af0529e8184db59d38a5df5
Teemu Saha <72320434+Tepes99@users.noreply.github.com>
2022-10-09 19:40:37 +0200

    .

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import dash
 import dash_bootstrap_components as dbc
 
 app = Dash(__name__, use_pages=True,external_stylesheets=[dbc.themes.LUX])
+server = app.server
 
 app.layout = html.Div([dash.page_container])
 
